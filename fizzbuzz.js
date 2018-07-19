20b444ce070f431b15ae3e813dab3d9e4cbaf392
Sam Worrall <samuel.worrall@hotmail.co.uk>
2018-07-19 12:36:08 +0100

    Refactor

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,9 +1,9 @@
 const fizzbuzz = (num) => {
   if(num % 15 === 0) {
     return 'fizzbuzz'
-  } else if(num === 3) {
+  } else if(num % 3 === 0) {
     return 'fizz'
-  } else if(num === 5) {
+  } else if(num % 5 === 0) {
     return 'buzz'
   }
 }
--- fizzbuzz.test.js
+++ fizzbuzz.test.js
@@ -11,3 +11,11 @@ test('Returns buzz', () => {
 test('Returns fizzbuzz', () => {
   expect(fizzbuzz(15)).toEqual('fizzbuzz');
 });
+
+test('Returns fizz', () => {
+  expect(fizzbuzz(9)).toEqual('fizz');
+});
+
+test('Returns buzz', () => {
+  expect(fizzbuzz(10)).toEqual('buzz');
+});
